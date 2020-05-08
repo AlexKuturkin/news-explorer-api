80b69639f46f4f858351cd115e5c11637b6a0938
Alex Kuturkin <alexkuturkin@mail.ru>
2020-05-08 10:15:44 +0300

    fix cookie

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,17 +3,16 @@ const { JWT_SECRET } = require('../config');
 const LoginError = require('../errors/loginError');
 const { errorAuthorization } = require('../constants/error-messages');
 
-module.exports.auth = (req, res, next) => {
-  /* res.set('Access-Control-Allow-Origin', 'http://localhost:8080')
-  res.set('Access-Control-Allow-Credentials', 'true') */
+module.exports = (req, res, next) => {
+  res.set('Access-Control-Allow-Origin', 'http://localhost:8080')
+  res.set('Access-Control-Allow-Credentials', 'true')
 
-  const { jwt: token } = req.cookies;
+  const { authorization } = req.headers;
 
-  if (!token) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new LoginError(errorAuthorization);
   }
-
-  // const token = authorization.replace('Bearer ', '');
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
